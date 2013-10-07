28797f3a1c720e6abc1c0a4acf7cf772b49c4464
Jim Stewart <jim@stewartstuff.com>
2013-10-06 20:04:32 -0500

    Initial Commit

---
+++ AdamGLTutorial2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AdamGLTutorial2
+//
+//  Created by James Stewart on 10/6/13.
+//  Copyright (c) 2013 Stewartstuff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
